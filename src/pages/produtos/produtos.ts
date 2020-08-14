de8fd0bc2247dbabaf3b04a40ed5a5d449f2a164
levimartines <levi.mferreira@gmail.com>
2020-08-14 15:58:22 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,12 @@
 import {Component} from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams, Refresher} from 'ionic-angular';
+import {
+  InfiniteScroll,
+  IonicPage,
+  LoadingController,
+  NavController,
+  NavParams,
+  Refresher
+} from 'ionic-angular';
 import {ProdutoDTO} from "../../models/produto.dto";
 import {ProdutoService} from "../../services/domain/produto.service";
 import {API_CONFIG} from "../../config/api.config";
@@ -10,7 +17,8 @@ import {API_CONFIG} from "../../config/api.config";
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController,
@@ -29,27 +37,33 @@ export class ProdutosPage {
     if (!categoria_id) {
       categoria_id = 1;
     }
+
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(res => {
-      this.items = res['content'];
+    this.produtoService.findByCategoria(categoria_id, this.page).subscribe(res => {
+      let start = this.items.length;
+      this.items = this.items.concat(res['content'] as ProdutoDTO[]);
+      let end = this.items.length - 1;
+      console.log(this.items)
       loader.dismiss().then();
-      this.loadImageUrl();
+      this.loadImageUrl(start, end);
     }, error => {
       loader.dismiss().then();
     });
   }
 
   showDetail(produto_id: string) {
-    this.navCtrl.push('ProdutoDetailPage', {produto_id});
+    this.navCtrl.push('ProdutoDetailPage', {produto_id}).then();
   }
 
-  loadImageUrl() {
-    this.items.forEach(item => {
+  loadImageUrl(start: number, end: number) {
+
+    for (let i = start; i <= end; i++) {
+      let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id).subscribe(res => {
         item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
       }, error => {
       });
-    })
+    }
   }
 
   presentLoading() {
@@ -61,9 +75,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher: Refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
-    }, 900);
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll: InfiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
   }
 }
